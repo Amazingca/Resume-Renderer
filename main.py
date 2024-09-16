596c5bc2637afd1f523bccf66a561767cd577616
Amazingca <67606943+Amazingca@users.noreply.github.com>
2024-09-15 20:10:09 -0600

    Add baseline data prototype for resume renderer

---
+++ main.py
@@ -0,0 +1,30 @@
+config = {
+    "vars": {
+        "title": "",
+        "isOnline": "",
+        "onlineDelimiter": "",
+        "delimiter": "",
+        "bullet": "",
+        "width": ""
+    },
+    "profile": {
+        "name": "",
+        "pronouns": "",
+        "email": "",
+        "phone": ""
+    },
+    "rows": [
+        {
+            "title": "",
+            "titleRight": "",
+            "description": "",
+            "descriptionRight": "",
+            "body": "",
+            "bullets": [
+                "",
+                ...
+            ]
+        },
+        ...
+    ]
+}
