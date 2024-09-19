4f68cf10c0e783823341bad865b1de2022271cde
Amazingca <67606943+Amazingca@users.noreply.github.com>
2024-09-18 18:07:54 -0600

    Add bullets, newline wrapping, finish up header, and other cool things

--- main.py
+++ main.py
@@ -69,16 +69,52 @@ def constructData():
 def sectionDivider(f):
     print(config["VARIABLES"]["delimiter"] * config["VARIABLES"]["width"], file=f)
 
+def formatEmail():
+    return config["PROFILE"]["email"].replace("@", config["VARIABLES"]["onlineDelimiter"] if config["VARIABLES"]["isOnline"] == "true" else "@")
+
 def outputHeader(f):
     for key in config["PROFILE"]:
-        if key == "email":
-            if config["VARIABLES"]["isOnline"] == "false":
+        match key:
+            case "name":
+                if "email" in config["PROFILE"]:
+                    print(f"""{config["PROFILE"][key]}{f"{formatEmail():>{config['VARIABLES']['width'] - len(config['PROFILE'][key])}}"}""", file=f)
+                    continue
+            case "phone":
+                if len(config["PROFILE"]) >= 4 and "pronouns" in config["PROFILE"]:
+                    print(f"""{config["PROFILE"]["pronouns"]}{f"{config['PROFILE'][key]:>{config['VARIABLES']['width'] - len(config['PROFILE']['pronouns'])}}"}""", file=f)
+            case "pronouns":
+                continue        
+            case "email":
+                continue
+            case _:
                 print(config["PROFILE"][key], file=f)
-            else:
-                print(config["PROFILE"][key].replace("@", config["VARIABLES"]["onlineDelimiter"]), file=f)
-        else:
-            print(config["PROFILE"][key], file=f)
+
     sectionDivider(f)
+
+def outputRows(f):
+    for row in config["ROWS"]:
+        titleRight = row['titleRight'] if 'titleRight' in row else ''
+        descriptionRight = row['descriptionRight'] if 'descriptionRight' in row else ''
+        print(f"""{row["title"].upper()}{f"{titleRight:>{config['VARIABLES']['width'] - len(row['title'])}}"}""", file=f)
+        print(f"""{row["description"]}{f"{descriptionRight:>{config['VARIABLES']['width'] - len(row['description'])}}"}""", file=f)
+        for line in row["body"].split("\\n"):
+            print("\n".join(cropWidth(line, "")), file=f) # Print each newline independant from cutoffs when cropping
+        for bullet in row["bullets"]:
+            print("\n".join(cropWidth(bullet, config['VARIABLES']['bullet'])), file=f)
+        sectionDivider(f)
+
+def cropWidth(line, newLineSpacer):
+    line = newLineSpacer + line
+    lines = []
+    while (len(line) > config["VARIABLES"]["width"]): # While we still have line overflow
+        indexSelection = config["VARIABLES"]["width"]
+        while (line[indexSelection - 1] != " "):
+            indexSelection -= 1 # Keep decremementing index until we can crop at a space
+        lineSelection = line[:indexSelection]
+        lines.append(lineSelection)
+        line = (" " * len(newLineSpacer)) + line[indexSelection:] # Add associated spacing, like if we're on bullets
+    lines.append(line)
+    return lines
 
 if __name__ == "__main__":
     constructData()
@@ -87,3 +123,4 @@ if __name__ == "__main__":
         print(config["VARIABLES"]["title"])
     with open(f"./{config['PROFILE']['name'].split(' ')[-1]}_Resume_{date.today()}.txt", "a") as f:
         outputHeader(f)
+        outputRows(f)
